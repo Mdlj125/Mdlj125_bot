200180d01dcbdac147fb16ff15fd9375e0f189f0
Mdlj125 <90634048+Mdlj125@users.noreply.github.com>
2021-09-13 11:49:00 -0500

    Create Mdlj125_bot.js

---
+++ Mdlj125_bot.js
@@ -0,0 +1,10 @@
+const { WAConnection } = require('@adiwajshing/baileys');
+const fs = require('fs');
+
+async function iniciar () { 
+        const client = new WAConnection()
+
+        client.logger.level = 'warn'
+
+        client.on('qr', () => {
+        })
